71ba9a3dbee860debb40874df0704d93fc54a8a3
Luke Wharton <onegluke@icloud.com>
2023-04-01 20:17:28 -0500

    Added Functions text cleaner and total func

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -7,6 +7,10 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 # Activation of the lemmatizer
 lemmatizer = WordNetLemmatizer()
+# Bringing the n-grams model from nltk
+from nltk.util import ngrams
+# Imports a counter to count the created ngrams
+from collections import Counter
 
 text = """They had no proof. He knew that they knew he had done it but they didn't have any proof. 
 It was a huge distinction and it was the difference between him keeping his freedom or being locked away for decades. 
@@ -14,17 +18,31 @@ They continued to question him, probing him for information that they could use 
 needed to put him away. He smiled and continued to block their every inquiry by feigning his innocence for a crime 
 they all knew he committed. """
 
-# This parses the text word by word in a list
-text_split_up = word_tokenize(text)
-# This then turns it into a 2d list buy appending what type of word to each list
-text_id = nltk.pos_tag(text_split_up)
 
-new_text_id = []
+# This is counts the total tokens in each text split by words, used later to compare to the ngram models to find a probability
+def token_total(text):
+    total = len(word_tokenize(text))
+    return total
 # Lemmatizes all words in the text, does a special lemmatizer for adjectives because they are missed by the regular settings
-for word in text_id:
-    if word[-1] == 'JJ':
-        new_text_id.append(lemmatizer.lemmatize(word[0], pos="a"))
-    else:
-        new_text_id.append(lemmatizer.lemmatize(word[0])) 
+def text_cleaner(text):
+    # This parses the text word by word in a list
+    text_split_up = word_tokenize(text)
+    # This then turns it into a 2d list buy appending what type of word to each list
+    text_id = nltk.pos_tag(text_split_up)   
+    new_text_id = []
+    for word in text_id:
+        if word[-1] == 'JJ':
+            new_text_id.append(lemmatizer.lemmatize(word[0], pos="a"))
+        else:
+            new_text_id.append(lemmatizer.lemmatize(word[0])) 
 
-print(new_text_id)
+    return new_text_id
+
+
+bigram_text = ngrams(text_cleaner(text), 2)
+bigram_text_frq = Counter(bigram_text)
+
+trigram_text = ngrams(text_cleaner(text), 3)
+trigram_text_frq = Counter(trigram_text)
+
+print(trigram_text_frq)
