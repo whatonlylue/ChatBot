df6a42475dc7b703011e2623a8e6a8d0d69ebc00
Luke Wharton <onegluke@icloud.com>
2023-04-11 14:45:00 -0400

    Updated all comments

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -15,10 +15,9 @@ import random
 
 
 # The corups is inputed in as a list, but must be a string inorder to lemmatize, so its turned into a string here
-text = " ".join(nltk.corpus.brown.words())
+text = " ".join(nltk.corpus.abc.words())
 
 
-# Lemmatizes all words in the text, does a special lemmatizer for adjectives because they are missed by the regular settings
 def text_cleaner(text):
     # This parses the text word by word in a list
     text_split_up = word_tokenize(text)
@@ -28,24 +27,35 @@ def text_cleaner(text):
             text_split_up.remove(word)
     return text_split_up
 
+
+# Parses the text again but in three string object, ex: the dog went to the park. [["The","dog","went"],["went","to","the"],["park"]]
 trigram_text = ngrams(text_cleaner(text), 3)
 trigram_text_frq = Counter(trigram_text)
 
+# Generates a random sentence based off of random selection
 def generate_text(ngram_text_frq):
     new_sentence = " "
+    # takes the parameter ngram and sorts it by its frequency
     new_ngram_ratio = ngram_text_frq.most_common()
     first_word = "the"
+
     while "." not in new_sentence:
+        # Creates a list comprehension of the all objects in the new_ngram_ratio, only including the ones starting with first_word 
         current_ngrams = [ngram for ngram in new_ngram_ratio if ngram[0][0] == first_word]
         next_word = random.choice(current_ngrams)
         new_sentence += " " + next_word[0][0] + " " + next_word[0][1]
+        # updates first word so it can recreate a new list comprehension from the new_ngram_ratio starting with the updated word
         first_word = next_word[0][-1]
 
+
+    # Cleans up the sentence by cuting off everything added by the period
     if "." in new_sentence:
         new_sentence = new_sentence[0:new_sentence.find(".")]
                     
-    print(new_sentence)
-        
-generate_text(trigram_text_frq)
+    return new_sentence
+
+
+# calls and prints the generate_text function with its specific perameter 
+print(generate_text(trigram_text_frq))
 
 
